7fd0e21a25f253cc0c8d58c5488b03469abfb9e2
kaveh <kaveh.sadidi@yahoo.com>
2019-11-30 20:34:55 +0330

    Pushng base "1st commit" ; Master branch

---
+++ csPrac/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* this is the base, my step by step practices would be on branches 
+                every branch is a practices of a certain subject ;) */
+
+        }
+    }
+}
