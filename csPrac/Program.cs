574f5e5b4ead3831de8673bcdbc88794b8bbb92a
caveh <kaveh.sadidi@yahoo.com>
2019-12-03 17:40:10 +0330

    Conversions, try/catch Prac

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -10,9 +10,34 @@ namespace csPrac
     {
         static void Main(string[] args)
         {
+            //implicit type Conversion (in conversion from smaller (ranged) datatypes to larger ones):
+            byte b1 = 1;
+            int i1 = b1;
 
-            /* this is the base, my step by step practices would be on branches 
-                every branch is a practices of a certain subject ;) */
+            //Explicit type conversion (in conversion from larger (ranged) datatypes to smaller ones) :
+            int i2 = 1;
+            byte b2 = (byte)i2;
+
+            //non-compatible datatype Conversion:
+            string s = "3";
+            int i3 = int.Parse(s); // or ==>  int i3 = Convert.ToInt32(s);
+
+
+            /*c# doesn't recognize overflow, unless we put checked code :
+              with this overflow will not happen at runtime instead an exception will be thrown
+              and program will crash unless U handle the exception & we handle it by trycatch. : */
+            try
+            {
+                checked
+                {
+                    byte b3 = 255;
+                    b3 += 1;
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Overflow occured: Byte range is 0 to 255");
+            }
 
         }
     }
