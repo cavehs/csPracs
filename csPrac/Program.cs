ed896f8592675296027a484d7c453f476b1dbb52
caveh <kaveh.sadidi@yahoo.com>
2019-12-03 20:44:11 +0330

    Operators: Arithmetic, Comparison, Assignment, Logical, Bitwise

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -11,9 +11,22 @@ namespace csPrac
         static void Main(string[] args)
         {
 
-            /* this is the base, my step by step practices would be on branches 
-                every branch is a practices of a certain subject ;) */
+            //Arithmetic Operations: +, -, *, /, %, ++, --
+            int a = 1, b = 3;
+            int d = ++a;
+            int e = a++;
+            Console.WriteLine("d:" + d + "      e:" + e + "       a:" + a);
 
+            //COmparison Operation (==, !=, >=, <=, <,>): 
+            Console.WriteLine("{0} {1} {2} {3} {4}", a == b, a > d, e < d, a != d, a >= 1);
+
+            //Assignment Operators(= ,+=, -=, *= , /=)
+            int m = a *= 1;
+
+            //Logical operators (||, &&, !)
+            Console.WriteLine();
+
+            //Bitwise OPerators (&, |) : used in low level programming
         }
     }
 }
