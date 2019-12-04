d6c89e80ac5afb78c87eb0063c58c13b09d70121
caveh <kaveh.sadidi@yahoo.com>
2019-12-04 19:16:48 +0330

    Array declaration Prac

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -11,8 +11,20 @@ namespace csPrac
         static void Main(string[] args)
         {
 
-            /* this is the base, my step by step practices would be on branches 
-                every branch is a practices of a certain subject ;) */
+            //int numbers1=1;
+            //int numbers2=2;
+            //int numbers3=3;
+            
+            int[] numbers = new int[3];      // var[] nnumbers = new int[3] { 1, 2, 3 };
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            //int[]    ->we tell compiler to create an array
+            //numbers  ->identifier or Name
+            //new      ->new operator allocates memory to an array.
+            //int[3]   ->size
+
 
         }
     }
