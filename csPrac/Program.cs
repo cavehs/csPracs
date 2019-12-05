6cb2724cc6b5cd1da7c5a52f821f8a381c12ee63
caveh <kaveh.sadidi@yahoo.com>
2019-12-05 21:37:20 +0330

    Enum must defined in Namespace level

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -6,29 +6,30 @@ using System.Threading.Tasks;
 
 namespace csPrac
 {
+
+    //we wanna build an app for post company & we wanna support few different shipping method  
+
+    /* 
+         _one approach is to declare this way below, although its work its messy:
+            const int RegularAirMail = 1;
+            const int RegisterAirMail = 2;
+            const int Express=3;
+    */
+
+
+    //we use enum to declare number of relative constants
+    //enum is internally an integer, we can change it by writing certain datatype infront of it
+    //if we don't set values to enum members,by deafult it's gonna set 1st member zero and increments it for each member,it's recommended to be initialized with a value.
+
+    public enum shippingMethod : byte  //becuase Enum is a new type(like class or structures) we need to define it in a namespace level
+    {
+        RegularAirMail = 1,
+        RegisterAirMail = 2,
+        Express = 3
+    };
+
     class Program
     {
-        //we wanna build an app for post company & we wanna support few different shipping method  
-
-        /* 
-             _one approach is to declare this way below, although its work its messy:
-                const int RegularAirMail = 1;
-                const int RegisterAirMail = 2;
-                const int Express=3;
-        */
-
-
-        //we use enum to declare number of relative constants
-        //enum is internally an integer, we can change it by writing certain datatype infront of it
-        //if we don't set values to enum members,by deafult it's gonna set 1st member zero and increments it for each member,it's recommended to be initialized with a value.
-
-        public enum shippingMethod: byte  //Enum can only be declared inside of a class, not method.
-        {
-            RegularAirMail = 1,
-            RegisterAirMail = 2,
-            Express = 3
-        };
-
         static void Main(string[] args)
         {
             var method = shippingMethod.Express;
