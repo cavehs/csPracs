54a7b9d5a9341ee08a4ed195cb899e1c598a5fff
caveh <kaveh.sadidi@yahoo.com>
2019-12-05 12:43:52 +0330

    Enum Prac

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -8,12 +8,33 @@ namespace csPrac
 {
     class Program
     {
+        //we wanna build an app for post company & we wanna support few different shipping method  
+
+        /* 
+             _one approach is to declare this way below, although its work its messy:
+                const int RegularAirMail = 1;
+                const int RegisterAirMail = 2;
+                const int Express=3;
+        */
+
+
+        //we use enum to declare number of relative constants
+        //enum is internally an integer, we can change it by writing infront of it
+        //if we don't set values to enum members,by deafult it's gonna set 1st member 1 and increment it for each member
+
+
         static void Main(string[] args)
         {
-
-            /* this is the base, my step by step practices would be on branches 
-                every branch is a practices of a certain subject ;) */
-
+            public enum shippingMethod
+            {
+            RegularAirMail = 1,
+            RegisterAirMail = 2,
+            Express = 3
+            }
         }
+
+        var method = shippingMethod.Express;
+        Console.WriteLine((int)metohde);
     }
 }
+
