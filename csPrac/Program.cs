f8bdb22fa87524aedd64e2e37f594ee420fab515
caveh <kaveh.sadidi@yahoo.com>
2019-12-06 21:49:51 +0330

    it returns a pressed key's ASCII Code

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -10,9 +10,9 @@ namespace csPrac
     {
         static void Main(string[] args)
         {
-
-            /* this is the base, my step by step practices would be on branches 
-                every branch is a practices of a certain subject ;) */
+            
+            var kp = Console.ReadKey();
+            Console.WriteLine((int)kp.KeyChar);
 
         }
     }
