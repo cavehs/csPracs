528d59c1adf7e8b24093576a9ac11784f7333314
caveh <kaveh.sadidi@yahoo.com>
2019-12-07 12:07:07 +0330

    Key to ASCII conversion added

--- csPrac/Program.cs
+++ csPrac/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Console = System.Console;
 
 namespace csPrac
 {
@@ -10,9 +11,21 @@ namespace csPrac
     {
         static void Main(string[] args)
         {
-            
-            var kp = Console.ReadKey();
-            Console.WriteLine((int)kp.KeyChar);
+            Console.WriteLine("Which one wou;d you choose: \n Enter 0 to open 'Key to ASCII' conversion\n Enter 1 to open 'ASCII to key' conversion");
+            var input = int.Parse(Console.ReadLine());
+
+            while (input == 0)
+            {
+                var kp = Console.ReadKey();
+                Console.WriteLine(": " + (int)kp.KeyChar);
+            }
+            while (input == 1) 
+            {
+                var code = int.Parse(Console.ReadLine());
+                Console.WriteLine((char)code);
+            }
+
+           
 
         }
     }
